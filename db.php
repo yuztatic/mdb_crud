f314a74f964bda473bff6fafb662258bece4e8c4
unknown <juliusmanipol@gmail.com>
2024-03-13 16:50:26 +0800

    updated parameters of pdo connection in db

--- db.php
+++ db.php
@@ -1,12 +1,12 @@
 <?php
 $databasePath = 'E:\\wamp\\www\\mdb\\dbUser.mdb'; // to be used in wamp
-$databasePath = 'C:\xampp\htdocs\mdb_crud\dbUser.mdb';// to be used in xampp
-// $dsn = 'odbc:Driver={Microsoft Access Driver (*.mdb,*.accdb)};Dbq=C:\xampp\htdocs\mdb_crud\dbUser.mdb';
-// $username = ''; // Your database username (if applicable)
-// $password = '5s6rDB*&6'; // Your database password (if applicable)
+
+//$databasePath = 'C:\xampp\htdocs\mdb_crud\dbUser.mdb';    // to be used in xampp
+
+$password = '5s6rDB*&6'; // Your database password (if applicable)
 
 try {
-    $pdo = new PDO("odbc:Driver={Microsoft Access Driver (*.mdb, *.accdb)};Dbq=$databasePath; uid=; pwd=5s6rDB*&6");
+    $pdo = new PDO("odbc:Driver={Microsoft Access Driver (*.mdb, *.accdb)};Dbq=$databasePath; uid=; pwd=$password");
     echo "Connected to the database successfully.";
 } catch (PDOException $e) {
     die('Connection failed: ' . $e->getMessage());
