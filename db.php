0021746f1d2f36d84376f5a86ec81ccc4dde99c2
unknown <juliusmanipol@gmail.com>
2024-03-13 16:47:58 +0800

    edited db.php

--- db.php
+++ db.php
@@ -1,5 +1,6 @@
 <?php
-$databasePath = 'C:\xampp\htdocs\mdb_crud\dbUser.mdb';
+$databasePath = 'E:\\wamp\\www\\mdb\\dbUser.mdb'; // to be used in wamp
+$databasePath = 'C:\xampp\htdocs\mdb_crud\dbUser.mdb';// to be used in xampp
 // $dsn = 'odbc:Driver={Microsoft Access Driver (*.mdb,*.accdb)};Dbq=C:\xampp\htdocs\mdb_crud\dbUser.mdb';
 // $username = ''; // Your database username (if applicable)
 // $password = '5s6rDB*&6'; // Your database password (if applicable)
